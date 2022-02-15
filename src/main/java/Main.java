4688d4d6c0e622b620ea6b6cd0f512c942039159
Elena Tolokova <elena89-06@mail.ru>
2022-02-15 17:22:41 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long bonus = service.calculate(1000_60, true);
 
-    service.calculate(amount, registered);
-  }
+        System.out.println(bonus);
+    }
 }
 
